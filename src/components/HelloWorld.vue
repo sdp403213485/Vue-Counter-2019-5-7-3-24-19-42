816001726b7781e38819bda1b6b77e0706a569fe
sdp <403213485@qq.com>
2019-09-02 10:30:29 +0800

    runok

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="counter">
+  <p>{{ number }}</p>
+  <button v-on:click="clickPlus">+</button>
+   <button v-on:click="clickMun">-</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+
+
+data(){
+  return{
+    number:0,
+    click1:"+",
+    click2:"-"
+  };
+},
+
+methods:{
+  clickPlus:function(){
+    this.number++;
+  },
+  clickMun:function(){
+    this.number--;
+  }
+}
+};
+</script>
+
+
+<style scoped>
+h1{
+  font-weight:normal;
+}
+</style>
